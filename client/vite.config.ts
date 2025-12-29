6fe5808853d80127cd9c807c7b0ef5847de0bb6f
lxve23 <anmol_thapa@outlook.com>
2025-12-28 20:58:27 -0500

    Update env config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  envDir: '../',
-  
+  // envDir: '../',
+
   server: {
     host: "::",
     port: 8084,
